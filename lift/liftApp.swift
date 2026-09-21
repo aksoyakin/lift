5a0d140331a44e83b352553bcb6d1ef2cb6a52c3
Akin Aksoy <akinaksoy@outlook.com.tr>
2026-09-21 17:10:41 +0300

    Initial Commit

---
+++ lift/liftApp.swift
@@ -0,0 +1,17 @@
+//
+//  liftApp.swift
+//  lift
+//
+//  Created by Akın Aksoy on 21.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct liftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
